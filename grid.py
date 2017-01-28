f43a1416a8de3239ff07d4f0cd32167f668c226b
nnickd <nicolas.dellitalia@gmail.com>
2017-01-27 22:43:45 -0500

    testing loop performance with time

--- grid.py
+++ grid.py
@@ -3,9 +3,11 @@ import numpy as np
 
 class grid(object):
 
-    def __init__(self, x, y, on=0.1, off=0.9):
+    def __init__(self, x, y, on=0.1, off=0.9, x_scale=1, y_scale=1):
         self.x = x
         self.y = y
+        self.x_scale = x_scale
+        self.y_scale = y_scale
         self.random_grid(on, off)
         self.on_list = []
 
@@ -38,21 +40,20 @@ class grid(object):
             return self.grid[(i+1) % self.y, (j-1) % self.x]
 
     def total_surrounding(self, i, j):
-        total = self.bounds(i, j, 'right')
-        total += self.bounds(i, j, 'left')
-        total += self.bounds(i, j, 'up')
-        total += self.bounds(i, j, 'down')
-        total += self.bounds(i, j, 'up-right')
-        total += self.bounds(i, j, 'up-left')
-        total += self.bounds(i, j, 'down-right')
-        total += self.bounds(i, j, 'down-left')
+        total = self.grid[i, (j+1) % self.x] + \
+                self.grid[i, (j-1) % self.x] + \
+                self.grid[(i-1) % self.y, j] + \
+                self.grid[(i+1) % self.y, j] + \
+                self.grid[(i-1) % self.y, (j+1) % self.x] + \
+                self.grid[(i-1) % self.y, (j-1) % self.x] + \
+                self.grid[(i+1) % self.y, (j+1) % self.x] + \
+                self.grid[(i+1) % self.y, (j-1) % self.x]
         return total / 255
 
     def apply_rules(self, i, j, buffer_grid):
         if self.on_at(i, j):
-            # self.on_list.append((i+600, j+600))
-            self.on_list.append(i+600)
-            self.on_list.append(j+600)
+            self.on_list.append((j * self.y_scale) + 960 - (self.x * self.x_scale / 2))
+            self.on_list.append((i * self.x_scale) + 540 - (self.y * self.y_scale / 2))
         total = self.total_surrounding(i, j)
         if (total < 2) or (total > 3):
             buffer_grid[i, j] = 0
--- render.py
+++ render.py
@@ -2,41 +2,49 @@ import pyglet
 import numpy as np
 from grid import grid
 from random import random
+from pyglet.window import mouse, key
+import time
 
 
 class render(object):
     window = pyglet.window.Window(fullscreen=True)
     vertex_list = None
-    n = 0.5
 
+    i = 0
+    t = time.time()
+    m = t
 
     def __init__(self):
-        self.grid = grid(100, 100, 0.4, 0.6)
-        self.grid.update()
-
-        self.amount = len(self.grid.on_list) // 2
-        colors = [(255, 0, 0) for i in range(self.amount)]
-        print(self.amount)
-        print(len(colors))
-
-        render.vertex_list = pyglet.graphics.vertex_list(self.amount, 'v2f/stream', 'c3B/static')
-        render.vertex_list.vertices = self.grid.on_list
-        render.vertex_list.colors = np.array(colors).flatten()
+        self.grid = grid(1920//10, 1080//10, 0.5, 0.5, 3, 3)
+        self.update(1)
 
         pyglet.clock.schedule_interval(self.update, 1/30)
         pyglet.app.run()
 
     def update(self, dt):
         self.grid.update()
-        print(self.grid.on_list)
         render.vertex_list = pyglet.graphics.vertex_list(len(self.grid.on_list) // 2, 'v2f/stream', 'c3B/static')
         render.vertex_list.vertices = self.grid.on_list
-        render.vertex_list.colors = np.array([(255, 0, 0) for i in range(len(self.grid.on_list) // 2)]).flatten()
+        render.vertex_list.colors = np.array([(0, 255, int(random() * 255)) for i in range(len(self.grid.on_list) // 2)]).flatten()
 
     @window.event
     def on_draw():
         render.window.clear()
         render.vertex_list.draw(pyglet.gl.GL_POINTS)
+        render.i += 1
+        if render.i % 10 == 0:
+            print(render.i, time.time() - render.m)
+            render.m = time.time()
+            if render.i == 100:
+                print(render.i, time.time() - render.t)
+                pyglet.app.exit()
+
+    @window.event
+    def on_key_press(symbol, modifiers):
+        if symbol == key.DOWN:
+            print(render.i)
+            print(time.time() - render.t)
+            pyglet.app.exit()
 
 
 r = render()
