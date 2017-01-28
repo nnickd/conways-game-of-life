5a050c0cfbd6fed44175380925dc2b7733f65890
nnickd <nicolas.dellitalia@gmail.com>
2017-01-28 15:49:00 -0500

    refactored for optomization and add blocks wih click

--- grid.py
+++ grid.py
@@ -3,13 +3,14 @@ import numpy as np
 
 class grid(object):
 
-    def __init__(self, x, y, on=0.1, off=0.9, x_scale=1, y_scale=1):
+    def __init__(self, screen_x, screen_y, x, y, on=0.1, off=0.9, scale=2, margin=2):
+        self.screen_x = screen_x
+        self.screen_y = screen_y
         self.x = x
         self.y = y
-        self.x_scale = x_scale
-        self.y_scale = y_scale
+        self.scale = scale
+        self.margin = margin
         self.random_grid(on, off)
-        self.on_list = []
 
     def random_grid(self, on, off):
         self.grid = np.random.choice([0, 255], self.x * self.y, p=[off, on]).reshape(self.y, self.x)
@@ -17,27 +18,6 @@ class grid(object):
     def add_glider(self, i, j):
         glider = np.array([[0, 0, 255], [255, 0, 255], [0, 255, 255]])
         self.grid[i:i+3, j:j+3] = glider
-
-    def on_at(self, i, j):
-        return True if self.grid[i, j] == 255 else False
-
-    def bounds(self, i, j, direction):
-        if direction == "right":
-            return self.grid[i, (j+1) % self.x]
-        elif direction == "left":
-            return self.grid[i, (j-1) % self.x]
-        elif direction == "up":
-            return self.grid[(i-1) % self.y, j]
-        elif direction == "down":
-            return self.grid[(i+1) % self.y, j]
-        elif direction == "up-right":
-            return self.grid[(i-1) % self.y, (j+1) % self.x]
-        elif direction == "up-left":
-            return self.grid[(i-1) % self.y, (j-1) % self.x]
-        elif direction == "down-right":
-            return self.grid[(i+1) % self.y, (j+1) % self.x]
-        elif direction == "down-left":
-            return self.grid[(i+1) % self.y, (j-1) % self.x]
 
     def total_surrounding(self, i, j):
         total = self.grid[i, (j+1) % self.x] + \
@@ -50,20 +30,31 @@ class grid(object):
                 self.grid[(i+1) % self.y, (j-1) % self.x]
         return total / 255
 
-    def apply_rules(self, i, j, buffer_grid):
-        if self.on_at(i, j):
-            self.on_list.append((j * self.y_scale) + 960 - (self.x * self.x_scale / 2))
-            self.on_list.append((i * self.x_scale) + 540 - (self.y * self.y_scale / 2))
+    def apply_rules(self, i, j):
+        if True if self.grid[i, j] == 255 else False:
+            self.on_list.append((j * self.scale) + (self.screen_x / 2) - (self.x * self.scale / 2))
+            self.on_list.append((i * self.scale) + (self.screen_y / 2) - (self.y * self.scale / 2))
+            self.on_list.append((j * self.scale) + (self.screen_x / 2) - (self.x * self.scale / 2) + self.margin - self.scale)
+            self.on_list.append((i * self.scale) + (self.screen_y / 2) - (self.y * self.scale / 2))
+            self.on_list.append((j * self.scale) + (self.screen_x / 2) - (self.x * self.scale / 2) + self.margin - self.scale)
+            self.on_list.append((i * self.scale) + (self.screen_y / 2) - (self.y * self.scale / 2) + self.margin - self.scale)
+            self.on_list.append((j * self.scale) + (self.screen_x / 2) - (self.x * self.scale / 2))
+            self.on_list.append((i * self.scale) + (self.screen_y / 2) - (self.y * self.scale / 2) + self.margin - self.scale)
+
         total = self.total_surrounding(i, j)
         if (total < 2) or (total > 3):
-            buffer_grid[i, j] = 0
+            self.buffer_grid[i, j] = 0
         elif total == 3:
-            buffer_grid[i, j] = 255
+            self.buffer_grid[i, j] = 255
 
     def update(self):
         self.on_list = []
-        buffer_grid = self.grid.copy()
+        self.buffer_grid = self.grid.copy()
         for i in range(self.y):
             for j in range(self.x):
-                self.apply_rules(i, j, buffer_grid)
-        self.grid[:] = buffer_grid[:]
+                self.apply_rules(i, j)
+
+        self.grid[:] = self.buffer_grid[:]
+
+    def turn_on(self, i, j):
+        self.grid[i, j] = 255
--- render.py
+++ render.py
@@ -7,44 +7,62 @@ import time
 
 
 class render(object):
-    window = pyglet.window.Window(fullscreen=True)
+    x = 100
+    y = 100
+    window = pyglet.window.Window(x, y)
+    # window = pyglet.window.Window(fullscreen=True)
     vertex_list = None
+    grid = grid(screen_x=x, screen_y=y, x=50, y=50, on=0.5, off=0.5, scale=2, margin=0)
 
     i = 0
     t = time.time()
     m = t
 
     def __init__(self):
-        self.grid = grid(1920//10, 1080//10, 0.5, 0.5, 3, 3)
         self.update(1)
 
         pyglet.clock.schedule_interval(self.update, 1/30)
         pyglet.app.run()
 
     def update(self, dt):
-        self.grid.update()
-        render.vertex_list = pyglet.graphics.vertex_list(len(self.grid.on_list) // 2, 'v2f/stream', 'c3B/static')
-        render.vertex_list.vertices = self.grid.on_list
-        render.vertex_list.colors = np.array([(0, 255, int(random() * 255)) for i in range(len(self.grid.on_list) // 2)]).flatten()
+        # t = time.time()
+
+        render.grid.update()
+        render.vertex_list = pyglet.graphics.vertex_list(len(render.grid.on_list) // 2, 'v2f/stream', 'c3B/static')
+        render.vertex_list.vertices = render.grid.on_list
+        render.vertex_list.colors = np.array([(0, 255, int(random() * 255)) for i in range(len(render.grid.on_list) // 2)]).flatten()
+
+        # if render.i % 10 == 0:
+        #     print(render.i, time.time() - render.t, render.i / (time.time() - render.t))
+        #     print(len(render.grid.on_list) // 2, time.time() - t)
+
+        render.i += 1
 
     @window.event
     def on_draw():
         render.window.clear()
-        render.vertex_list.draw(pyglet.gl.GL_POINTS)
-        render.i += 1
-        if render.i % 10 == 0:
-            print(render.i, time.time() - render.m)
-            render.m = time.time()
-            if render.i == 100:
-                print(render.i, time.time() - render.t)
-                pyglet.app.exit()
+        render.vertex_list.draw(pyglet.gl.GL_QUADS)
+
+        # if render.i == 1000:
+        #     print(render.i, time.time() - render.t, render.i / (time.time() - render.t))
+        #     pyglet.app.exit()
 
     @window.event
     def on_key_press(symbol, modifiers):
         if symbol == key.DOWN:
-            print(render.i)
+            print(render.i, time.time() - render.m)
             print(time.time() - render.t)
             pyglet.app.exit()
+
+    @window.event
+    def on_mouse_press(x, y, button, modifiers):
+        if button == mouse.LEFT:
+            j = (x + (render.grid.x * render.grid.scale // 2) - (render.x / 2)) // render.grid.scale + 1
+            i = (y + (render.grid.y * render.grid.scale // 2) - (render.y / 2)) // render.grid.scale + 1
+            render.grid.turn_on(i, j)
+
+        if button == mouse.RIGHT:
+            pass
 
 
 r = render()
